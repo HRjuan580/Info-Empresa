ef1a608915c56517c0d76896f28443c30a9d07cc
HRJuan580 <jherrel580@g.educaand.es>
2025-01-27 12:56:21 +0100

    modo oscuro con js

--- otraspaginas/cambiarmodo.js
+++ otraspaginas/cambiarmodo.js
@@ -1,8 +1,12 @@
-const body = document.querySelector('body');
-const cambiarmodo = document.getElementById('.cambiarmodo');
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
 
-body.classList.toggle('cambiarmodo', localStorage.getItem('mode') === 'oscuro');
+    var button = document.getElementById("modeOscuro");
+    if(document,body.classList.contains('modeOscuro')){
+        button.textContent = '🌓';
+    }
+    else {
+        button.textContent = '🌑';
+    }
+}
 
-cambiarmodo.addEventListener('click', (e) => {})
-    const isoscuro = body.classList.toggle('isoscuro');
-    localStorage.setItem('mode', isoscuro ? 'oscuro' : 'light');
